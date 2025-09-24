d4568c6c5fdc4c0e47b02148493080850ceeb959
Yolkee <23.Basov.V@gmail.com>
2025-09-25 02:02:13 +0300

    1 part of torrent projcet

--- project-part-1/torrent-file/task.h
+++ project-part-1/torrent-file/task.h
@@ -9,12 +9,13 @@
 #include <map>
 #include <sstream>
 #include <iostream>
-#include<cstdio>
+#include <cstdio>
 #include <cstdint>
 #include <any>
-#include<string>
+#include <string>
 #include <utility>
 #include <memory>
+
 
 void sha1(const char *data, size_t length, unsigned char *hash) {
     SHA1((const unsigned char *) data, length, hash);
@@ -30,22 +31,14 @@ struct TorrentFile {
     std::string infoHash;
 };
 
-/*
- * Функция парсит .torrent файл и загружает информацию из него в структуру `TorrentFile`. Как устроен .torrent файл, можно
- * почитать в открытых источниках (например http://www.bittorrent.org/beps/bep_0003.html).
- * После парсинга файла нужно также заполнить поле `infoHash`, которое не хранится в файле в явном виде и должно быть
- * вычислено. Алгоритм вычисления этого поля можно найти в открытых источника, как правило, там же,
- * где описание формата .torrent файлов.
- * Данные из файла и infoHash будут использованы для запроса пиров у торрент-трекера. Если структура `TorrentFile`
- * была заполнена правильно, то трекер найдет нужную раздачу в своей базе и ответит списком пиров. Если данные неверны,
- * то сервер ответит ошибкой.
- */
-
 class BenCode
 {
 public:
     virtual ~BenCode() = default;
-    virtual std::string getstr() const = 0;
+    virtual std::string getStr() const = 0;
+    static std::shared_ptr<BenCode> parse(const char* data, size_t length, size_t& pos);
+    //virtual const int64_t getData() const = 0;
+    //virtual const std::string getData() const = 0;
 };
 
 class BenCode_List : public BenCode
@@ -53,79 +46,156 @@ class BenCode_List : public BenCode
 public:
     std::vector<std::shared_ptr<BenCode>> list_data_;
 
-    uint64_t size() const noexcept
+    uint64_t getSize() const noexcept
     {
         return list_data_.size();
     }
 
-    std::shared_ptr<BenCode> get_l(size_t  index) const
+    std::shared_ptr<BenCode> getValue(size_t  index) const
     {
         if(index >= list_data_.size()){
-            throw std::runtime_error("Invalid bencode: index out of rangeqqq");
+            throw std::runtime_error("Invalid bencode: index out of range");
         }
         return list_data_[index];
     }
 
-    std::string getstr() const noexcept
-    {
-        std::string sub_ans = "";
-        for(auto& el : list_data_)
-        {
-            sub_ans += el->getstr();
-        }
-        return "l" + sub_ans + "e";
+    void addValue(std::shared_ptr<BenCode> element) {
+        list_data_.push_back(std::move(element));
     }
-};
 
+    std::string getStr() const noexcept
+    {
+        std::string res = "";
+        for(const auto& el : list_data_)
+        {
+            res += el->getStr();
+        }
+        return "l" + res + "e";
+    }
+
+    static std::shared_ptr<BenCode_List> parse(const char* data, size_t length, size_t& pos){
+        if (pos >= length || data[pos] != 'l') {
+            throw std::runtime_error("Expected list 'l' at position " + std::to_string(pos) + " while parsing list values");
+        }
+
+        pos++;
+        auto list = std::make_shared<BenCode_List>();
+        while(pos < length && data[pos] != 'e')
+        {
+            auto new_element = BenCode::parse(data, length, pos);
+            list->addValue(std::move(new_element));
+        }
+
+        if(pos >= length)
+        {
+            throw std::runtime_error("Unterminated list: missing 'e'");
+        }
+        pos++;
+        return list;
+    }
+
+};
 
 class BenCode_Integer : public BenCode
 {
-public:
+private:
     int64_t data_;
-    BenCode_Integer(long long data): data_(data){}
-    long long get_i() const noexcept
+public:
+    BenCode_Integer(int64_t data): data_(data){}
+    
+    int64_t getData() const noexcept //override
     {
         return data_;
     }
 
-    std::string getstr() const noexcept
+    std::string getStr() const noexcept
     {
         return "i" + std::to_string(data_) + "e";
+    }
+
+    static std::shared_ptr<BenCode_Integer> parse(const char* data, size_t length, size_t& pos) {
+        if (pos >= length || data[pos] != 'i') {
+            throw std::runtime_error("Expected integer 'i' at position " + std::to_string(pos) + " while parsing integer value");
+        }
+        pos++;
+        const size_t start_pos = pos;
+        
+        while (pos < length && data[pos] != 'e') {
+            if (pos == start_pos && data[pos] == '-') {
+                pos++;
+                continue;
+            }
+            if (!std::isdigit(data[pos])) {
+                throw std::runtime_error("Invalid integer character");
+            }
+            pos++;
+        }
+        
+        if (pos >= length) throw std::runtime_error("Unterminated integer");
+        
+        std::string number_str(data + start_pos, pos - start_pos);
+        pos++;
+        
+        if (number_str.empty()) throw std::runtime_error("Empty integer");
+        if (number_str == "-0") throw std::runtime_error("Negative zero");
+        if (number_str.size() > 1 && number_str[0] == '0') throw std::runtime_error("Leading zero");
+        
+        return std::make_shared<BenCode_Integer>(std::stoll(number_str));
     }
 };
 
 class BenCode_String : public BenCode
 {
-public:
+private:
     std::string data_;
+public:
     BenCode_String(std::string data) : data_(data) {}
 
-    std::string get_s() const noexcept
+    std::string getData() const noexcept//override
     {
         return data_;
     }
 
-    std::string getstr() const noexcept override
+    std::string getStr() const noexcept override
     {
         return std::to_string(data_.length()) + ":" + data_;
     }
 
+    static std::shared_ptr<BenCode_String> parse(const char* data, size_t length, size_t& pos) {
+        if (pos >= length || !std::isdigit(data[pos])) {
+            throw std::runtime_error("Expected string at position " + std::to_string(pos) + " while parsing string value");
+        }
+        
+        const size_t start_pos = pos;
+        while (pos < length && data[pos] != ':') {
+            if (!std::isdigit(data[pos])) throw std::runtime_error("Invalid string length");
+            pos++;
+        }
+        
+        if (pos >= length) throw std::runtime_error("Unterminated string length");
+        
+        std::string length_str(data + start_pos, pos - start_pos);
+        size_t str_length = std::stoull(length_str);
+        pos++;
+                
+        std::string value(data + pos, str_length);
+        pos += str_length;
+        
+        return std::make_shared<BenCode_String>(value);
+    }
 };
-
 
 struct BenCode_Dictionary: public BenCode
 {
-public:
+private:
     std::map<std::string, std::shared_ptr<BenCode>> dict_data_;
-
-    uint64_t size() const noexcept
+public:
+    uint64_t getSize() const noexcept
     {
         return dict_data_.size();
     }
 
-
-    std::shared_ptr<BenCode> get(const std::string &key) const
-    {
+    std::shared_ptr<BenCode> getValue(const std::string &key) const{
         if(dict_data_.find(key) == dict_data_.end())
         {
             throw std::runtime_error("Invalid bencode: no key " + key);
@@ -133,143 +203,116 @@ public:
         return dict_data_.find(key)->second;
     }
 
-    std::string getstr() const noexcept override
-    {
+    std::string getStr() const noexcept override{
         std::string sub_ans = "";
-        for(auto& el : dict_data_)
+        for(const auto& el : dict_data_)
         {
-            sub_ans += BenCode_String(el.first).getstr() + el.second->getstr();
+            sub_ans += BenCode_String(el.first).getStr() + el.second->getStr();
         }
 
-
         return "d" + sub_ans + "e";
+    }
+
+    void setValue(const std::string& key, std::shared_ptr<BenCode> value) {
+        dict_data_[key] = std::move(value);
+    }
+
+    static std::shared_ptr<BenCode_Dictionary> parse(const char* data, size_t length, size_t& pos){
+        if(pos >= length || data[pos] != 'd'){
+            throw std::runtime_error("Expected dictionary 'd' at position " + std::to_string(pos));
+        }
+
+        pos++;
+        auto dict = std::make_shared<BenCode_Dictionary>();
+
+        while(pos < length && data[pos] != 'e')
+        {
+            auto key_string = BenCode_String::parse(data, length, pos);
+            if (!key_string) {
+                throw std::runtime_error("Dictionary key must be a string at position " + std::to_string(pos));
+            }
+
+            if (pos >= length) {
+                throw std::runtime_error("Unexpected end of input in dictionary");
+            }
+            auto value_obj = BenCode::parse(data, length, pos);
+            dict->setValue(key_string->getData(), std::move(value_obj));
+        }
+        if (pos >= length) {
+            throw std::runtime_error("Unterminated dictionary: missing 'e'");
+        }
+
+        pos++;
+        return dict;
     }
 
 };
 
 
-std::shared_ptr<BenCode> parse_fucking_BenCode(const char* ben, uint64_t len, uint64_t &pos)
-{
-    char x = ben[pos++];
-    std::shared_ptr<BenCode> res;
-    if(x == 'l') {
-        res = std::make_shared<BenCode_List>();
-        while (ben[pos] != 'e') {
-            std::dynamic_pointer_cast<BenCode_List>(res)->list_data_.push_back(parse_fucking_BenCode(ben, len, pos));
-        }
-        ++pos;
-        return res;
+std::shared_ptr<BenCode> BenCode::parse(const char* data, size_t length, size_t& pos) {
+    if (pos >= length) {
+        throw std::runtime_error("Unexpected end of input at position " + std::to_string(pos));
     }
-        else if(x == 'i')
-        {
-            uint64_t pos1 = pos;
-            while(pos1 < len && ben[pos1] != 'e')
-            {
-                ++pos1;
+    
+    const char current_char = data[pos];
+    
+    switch (current_char) {
+        case 'i':
+            return BenCode_Integer::parse(data, length, pos);
+        case 'l':
+            return BenCode_List::parse(data, length, pos);
+        case 'd':
+            return BenCode_Dictionary::parse(data, length, pos);
+        default:
+            if (std::isdigit(current_char)) {
+                return BenCode_String::parse(data, length, pos);
             }
-            if(pos1 >= len)
-            {
-                throw std::runtime_error("Invalid bencode: index out of rangeaaa");;
-            }
-            std::string s(ben + pos, pos1 - pos);
-            res = std::make_shared<BenCode_Integer>(BenCode_Integer(std::stoll(s)));
-            pos = pos1 + 1;
-            return res;
-        }
+            throw std::runtime_error(std::string("Unexpected character '") + current_char + 
+                                   "' at position " + std::to_string(pos));
+    }
+}
 
+std::string GetInfoHash(const BenCode_Dictionary& FullInfo) {
+    std::string info = FullInfo.getValue("info")->getStr();
 
-        else if(x == 'd')
-        {
-            res = std::make_shared<BenCode_Dictionary>();
-            while(ben[pos] != 'e')
-            {
-                std::string key = std::dynamic_pointer_cast<BenCode_String>(parse_fucking_BenCode(ben,len,pos))->data_;
-                std::dynamic_pointer_cast<BenCode_Dictionary>(res)->dict_data_[key] = parse_fucking_BenCode(ben,len,pos);
-            }
-            pos++;
-            return res;
-        }
-        else
-        {
-            uint64_t pos1 = pos - 1;
-            while(pos1 < len && ben[pos1] != ':')
-            {
-                if(ben[pos1] == 'e')
-                {
-                    throw std::runtime_error("Invalid bencode: index out of rangezzz");;
-                }
-                pos1++;
-            }
-            if(pos1 >= len)
-            {
-                throw std::runtime_error("Invalid bencode: unexpected end of input");
-            }
-            std::string ans(ben + pos - 1, pos1 - pos + 1);
-            size_t len = std::stoll(ans);
-            pos = pos1 + 1;
-            res = std::make_shared<BenCode_String>(std::string(ben + pos, len));
-            pos += len;
-            return res;
-        }
+    unsigned char hash_arr[20];
+    SHA1((const unsigned char *)info.c_str(), info.length(), hash_arr);
+    std::string res = "";
+    for (int i = 0; i < 20; i++) {
+        res += hash_arr[i];
     }
 
-    std::string do_hash(const BenCode_Dictionary& dict){
-        if (dict.dict_data_.find("info") == dict.dict_data_.end()) {
-            throw std::runtime_error("Invalid bencode: no info dictionary");
-        }
-        std::string subres = dict.get("info")->getstr();
-        unsigned char arr[20];
-        sha1(subres.c_str(), subres.length(), arr);
-        std::string result = "";
-        for (int i = 0; i < 20; i++) {
-            result += arr[i];
-        }
-        return result;
+    return res;
+}
+
+std::vector<std::string> GetPieceHashes(const std::shared_ptr<BenCode_Dictionary>& dict){
+    std::string s = dynamic_pointer_cast<BenCode_String>(dynamic_pointer_cast<BenCode_Dictionary>(dict->getValue("info"))->getValue("pieces"))->getData();
+
+    std::vector<std::string> result;
+    for (int i = 0; i < s.length(); i += 20) {
+        result.push_back(s.substr(i, 20));
     }
-
-    std::string GetInfoHash(const BenCode_Dictionary& FullInfo){
-        std::string info = FullInfo.get("info")->getstr();
-
-        unsigned char hash[20];
-        SHA1((const unsigned char *)info.c_str(), info.length(), hash);
-        std::string result;
-        for (int i = 0; i < 20; i++) {
-            result += hash[i];
-        }
-
-        return result;
-    }
-
-    std::vector<std::string> GetPieceHashes(const std::shared_ptr<BenCode_Dictionary>& nig){
-        std::string s = dynamic_pointer_cast<BenCode_String>(dynamic_pointer_cast<BenCode_Dictionary>(nig->get("info"))->get("pieces"))->get_s();
-
-        std::vector<std::string> result;
-        for (int i = 0; i < s.length(); i += 20) {
-            result.push_back(s.substr(i, 20));
-        }
-        return result;
-    }
+    return result;
+}
 
 
+TorrentFile LoadTorrentFile(const std::string& filename) {
+    std::ifstream file(filename, std::ios::binary);
+    std::stringstream buf;
+    buf << file.rdbuf();
+    std::string data = buf.str();
+    file.close();
 
+    TorrentFile TrFile;
+    size_t position = 0;
 
+    auto root_dict = BenCode_Dictionary::parse(data.c_str(),data.size(),position);
+    TrFile.announce = std::dynamic_pointer_cast<BenCode_String>(root_dict->getValue("announce"))->getData();
+    TrFile.comment = std::dynamic_pointer_cast<BenCode_String>(root_dict->getValue("comment"))->getData();
+    TrFile.pieceLength = std::dynamic_pointer_cast<BenCode_Integer>(std::dynamic_pointer_cast<BenCode_Dictionary>(root_dict->getValue("info"))->getValue("piece length"))->getData();
+    TrFile.length = std::dynamic_pointer_cast<BenCode_Integer>(std::dynamic_pointer_cast<BenCode_Dictionary>(root_dict->getValue("info"))->getValue("length"))->getData();
+    TrFile.pieceHashes = GetPieceHashes(root_dict);
+    TrFile.infoHash = GetInfoHash(*root_dict);
+    return TrFile;
+}
 
-    TorrentFile LoadTorrentFile(const std::string& file) {
-        std::ifstream z(file, std::ios::binary);
-        std::stringstream b;
-        b << z.rdbuf();
-        std::string data = b.str();
-        z.close();
-
-        TorrentFile Ans;
-        uint64_t position = 0;
-
-        std::shared_ptr<BenCode_Dictionary> dict = std::dynamic_pointer_cast<BenCode_Dictionary>(parse_fucking_BenCode(data.c_str(),data.size(),position));
-        Ans.announce = std::dynamic_pointer_cast<BenCode_String>(dict->get("announce"))->get_s();
-        Ans.comment = std::dynamic_pointer_cast<BenCode_String>(dict->get("comment"))->get_s();
-        Ans.pieceLength = std::dynamic_pointer_cast<BenCode_Integer>(std::dynamic_pointer_cast<BenCode_Dictionary>(dict->get("info"))->get("piece length"))->get_i();
-        Ans.length = std::dynamic_pointer_cast<BenCode_Integer>(std::dynamic_pointer_cast<BenCode_Dictionary>(dict->get("info"))->get("length"))->get_i();
-        Ans.pieceHashes = GetPieceHashes(dict);
-        Ans.infoHash = GetInfoHash(*dict);
-        return Ans;
-    }
